51846112d07a1cba1445721ccbd1b80459a0e7bf
Kesler Mathieu <kesmathieu30@gmail.com>
2021-10-27 20:04:50 -0400

    adding inital cmd for mystery

--- CS50_SQL/fiftyville/log.sql
+++ CS50_SQL/fiftyville/log.sql
@@ -1,2 +1,15 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+
+-- Initial starting point!
+SELECT description
+FROM   crime_scene_reports
+WHERE  street = 'Chamberlin Street'
+       AND month = 7
+       AND day = 28
+       AND year = 2020 ;
+------------------------
+-- RESULTS
+-- Theft of the CS50 duck took place at 10:15am at the Chamberlin Street courthouse.
+-- Interviews were conducted today with three witnesses who were present at the time — each of their interview transcripts mentions the courthouse.
+-------------------------
