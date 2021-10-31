c268f4dc2b11a429e0511f8345aab105d5d80143
Kesler Mathieu <kesmathieu30@gmail.com>
2021-10-31 00:42:29 -0400

    adding confirmation query

--- CS50_SQL/fiftyville/log.sql
+++ CS50_SQL/fiftyville/log.sql
@@ -16,7 +16,7 @@ WHERE  street = 'Chamberlin Street'
        AND day = 28
        AND year = 2020 ;
 ------------------------------------------------------------------------
--- RESULTS
+-- RESULTS:
 -- Theft of the CS50 duck took place at 10:15am at the Chamberlin Street courthouse.
 -- Interviews were conducted today with three witnesses who were present at the time — each of their interview transcripts mentions the courthouse.
 -------------------------------------------------------------------------
@@ -24,7 +24,7 @@ WHERE  street = 'Chamberlin Street'
 -- REASONING: this is a listing of all those who entered the courthouse prior to 10:15am on July 28th
 -- NOTE: Query has also been sanitized of those who have exited before that time to reduce scope
 
-SELECT *
+ SELECT *
 FROM   courthouse_security_logs
 WHERE  month = 7
        AND day = 28
@@ -51,20 +51,79 @@ WHERE  month = 7
        AND day = 28
        AND hour >= 10
        AND minute > 14
-       AND activity = 'entrance';
+       AND activity = 'entrance'
+ORDER  BY license_plate;
 
 ------------------------------------------------------------------------
--- RESULTS
+-- RESULTS:
 --id    year  mo.   day hr. min. activity   lic. plate
---231	2020	7	28	8	18	entrance	L93JTIZ
---232	2020	7	28	8	23	entrance	94KL13X
---237	2020	7	28	8	34	entrance	1106N58
---240	2020	7	28	8	36	entrance	322W7JE
 --243	2020	7	28	8	42	entrance	0NTHK55
+--237	2020	7	28	8	34	entrance	1106N58
+--259	2020	7	28	10	14	entrance	13FNH73
+--240	2020	7	28	8	36	entrance	322W7JE
 --254	2020	7	28	9	14	entrance	4328GD8
 --255	2020	7	28	9	15	entrance	5P2BI95
 --256	2020	7	28	9	20	entrance	6P58WS2
+--232	2020	7	28	8	23	entrance	94KL13X
 --257	2020	7	28	9	28	entrance	G412CB7
+--231	2020	7	28	8	18	entrance	L93JTIZ
 --258	2020	7	28	10	8	entrance	R3G7486
---259	2020	7	28	10	14	entrance	13FNH73
+-------------------------------------------------------------------------
+
+
+-- CONFIRMATION: Confirming all those people left after 10:15am on July 28, 2020
+
+ SELECT *
+FROM   courthouse_security_logs
+WHERE  license_plate IN (SELECT license_plate
+                         FROM   courthouse_security_logs
+                         WHERE  month = 7
+                                AND day = 28
+                                AND hour <= 10
+                                AND activity = 'entrance'
+                                AND license_plate NOT IN (SELECT license_plate
+                                                          FROM
+                                    courthouse_security_logs
+                                                          WHERE  month = 7
+                                                                 AND day = 28
+                                                                 AND hour <= 10
+                                                                 AND activity =
+                                                                     'exit'
+                                                          EXCEPT
+                                                          SELECT license_plate
+                                                          FROM
+                                    courthouse_security_logs
+                                                          WHERE  month = 7
+                                                                 AND day = 28
+                                                                 AND hour >= 10
+                                                                 AND minute > 14
+                                                                 AND activity =
+                                                                     'exit')
+                         EXCEPT
+                         SELECT license_plate
+                         FROM   courthouse_security_logs
+                         WHERE  month = 7
+                                AND day = 28
+                                AND hour >= 10
+                                AND minute > 14
+                                AND activity = 'entrance')
+       AND month = 7
+       AND day = 28
+       AND activity = 'exit'
+ORDER  BY license_plate;
+
+------------------------------------------------------------------------
+-- CONFIRMATION RESULTS:
+--id    year  mo.   day hr. min. activity   lic. plate
+--267	2020	7	28	10	23	  exit      0NTHK55
+--268	2020	7	28	10	35	  exit      1106N58
+--288	2020	7	28	17	15	  exit	    13FNH73
+--266	2020	7	28	10	23	  exit	    322W7JE
+--263	2020	7	28	10	19	  exit	    4328GD8
+--260	2020	7	28	10	16	  exit    	5P2BI95
+--262	2020	7	28	10	18	  exit    	6P58WS2
+--261	2020	7	28	10	18	  exit    	94KL13X
+--264	2020	7	28	10	20	  exit    	G412CB7
+--265	2020	7	28	10	21	  exit    	L93JTIZ
+--290	2020	7	28	17	18	  exit	    R3G7486
 -------------------------------------------------------------------------
