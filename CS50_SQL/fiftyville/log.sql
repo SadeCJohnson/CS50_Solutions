f8af88d1f7ec29a3907c92e9eb7a0e9d634ab9ca
Kesler Mathieu <kesmathieu30@gmail.com>
2021-10-31 00:25:37 -0400

    adding commentary

--- CS50_SQL/fiftyville/log.sql
+++ CS50_SQL/fiftyville/log.sql
@@ -15,11 +15,11 @@ WHERE  street = 'Chamberlin Street'
        AND month = 7
        AND day = 28
        AND year = 2020 ;
-------------------------
+------------------------------------------------------------------------
 -- RESULTS
 -- Theft of the CS50 duck took place at 10:15am at the Chamberlin Street courthouse.
 -- Interviews were conducted today with three witnesses who were present at the time — each of their interview transcripts mentions the courthouse.
--------------------------
+-------------------------------------------------------------------------
 
 -- REASONING: this is a listing of all those who entered the courthouse prior to 10:15am on July 28th
 -- NOTE: Query has also been sanitized of those who have exited before that time to reduce scope
@@ -52,3 +52,19 @@ WHERE  month = 7
        AND hour >= 10
        AND minute > 14
        AND activity = 'entrance';
+
+------------------------------------------------------------------------
+-- RESULTS
+--id    year  mo.   day hr. min. activity   lic. plate
+--231	2020	7	28	8	18	entrance	L93JTIZ
+--232	2020	7	28	8	23	entrance	94KL13X
+--237	2020	7	28	8	34	entrance	1106N58
+--240	2020	7	28	8	36	entrance	322W7JE
+--243	2020	7	28	8	42	entrance	0NTHK55
+--254	2020	7	28	9	14	entrance	4328GD8
+--255	2020	7	28	9	15	entrance	5P2BI95
+--256	2020	7	28	9	20	entrance	6P58WS2
+--257	2020	7	28	9	28	entrance	G412CB7
+--258	2020	7	28	10	8	entrance	R3G7486
+--259	2020	7	28	10	14	entrance	13FNH73
+-------------------------------------------------------------------------
