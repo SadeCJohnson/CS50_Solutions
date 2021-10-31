f93310164f0fd1ccfcf2866cd5eea4c3f9305108
Kesler Mathieu <kesmathieu30@gmail.com>
2021-10-31 00:22:59 -0400

    fixing 2nd sql step

--- CS50_SQL/fiftyville/log.sql
+++ CS50_SQL/fiftyville/log.sql
@@ -21,12 +21,29 @@ WHERE  street = 'Chamberlin Street'
 -- Interviews were conducted today with three witnesses who were present at the time — each of their interview transcripts mentions the courthouse.
 -------------------------
 
+-- REASONING: this is a listing of all those who entered the courthouse prior to 10:15am on July 28th
+-- NOTE: Query has also been sanitized of those who have exited before that time to reduce scope
+
 SELECT *
 FROM   courthouse_security_logs
 WHERE  month = 7
        AND day = 28
        AND hour <= 10
        AND activity = 'entrance'
+       AND license_plate NOT IN (SELECT license_plate
+                                 FROM   courthouse_security_logs
+                                 WHERE  month = 7
+                                        AND day = 28
+                                        AND hour <= 10
+                                        AND activity = 'exit'
+                                 EXCEPT
+                                 SELECT license_plate
+                                 FROM   courthouse_security_logs
+                                 WHERE  month = 7
+                                        AND day = 28
+                                        AND hour >= 10
+                                        AND minute > 14
+                                        AND activity = 'exit')
 EXCEPT
 SELECT *
 FROM   courthouse_security_logs
@@ -35,5 +52,3 @@ WHERE  month = 7
        AND hour >= 10
        AND minute > 14
        AND activity = 'entrance';
-------------------------
--- RESULTS: this is a listing of all those who entered the courthouse prior to 10:15am on July 28th ... need to minus those who have exited before that time to reduce scope
