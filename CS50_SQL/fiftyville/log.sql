59917e3cc7561d23a7dd95c4b52b552f76e1a0f1
Kesler Mathieu <kesmathieu30@gmail.com>
2021-11-01 04:43:16 -0400

    adding query to uncover final 2 suspects

--- CS50_SQL/fiftyville/log.sql
+++ CS50_SQL/fiftyville/log.sql
@@ -271,7 +271,9 @@ WHERE  activity = 'exit'
 -------------------------------------------------------------------------
 
 -- This query was formulated on the tip that the thief called someone
--- for less than a minute
+-- for less than a minute.
+-- As a recap, this query returns those who called and had a convo less than 1 minute
+-- after leaving the scene and then drove off within 10 min after crime
 SELECT people.NAME,
        passport_number,
        phone_calls.caller,
@@ -308,24 +310,64 @@ WHERE  caller IN (SELECT phone_number
 --Russell	3592750733	(770) 555-1861	(725) 555-3243	2020	28	7	49
 
 ------------------------------------------------------------------------
--- This query was formulated on the tip that the thief withdrew on Fifer Street
--- on the day of the crime
+-- this query returns the names of those who withdrew at Fifer Street
+-- the day of the incident
 SELECT *
-FROM   atm_transactions
-WHERE  year = 2020
-       AND month = 7
-       AND day = 28
-       AND transaction_type = 'withdraw'
-       AND atm_location = 'Fifer Street' ;
+FROM   bank_accounts
+       INNER JOIN people
+               ON people.id = bank_accounts.person_id
+WHERE  account_number IN (SELECT account_number
+                          FROM   atm_transactions
+                          WHERE  year = 2020
+                                 AND month = 7
+                                 AND day = 28
+                                 AND transaction_type = 'withdraw'
+                                 AND atm_location = 'Fifer Street');
 
 ------------------------------------------------------------------------
---RESULTS: when we uncover thief, we shall backtrack here as the accomplice is receiver!
---[id, account_number, year, month, day, atm_location, transaction_type, amount]
---246	28500762	2020	7	28	Fifer Street	withdraw	48
---264	28296815	2020	7	28	Fifer Street	withdraw	20
---266	76054385	2020	7	28	Fifer Street	withdraw	60
---267	49610011	2020	7	28	Fifer Street	withdraw	50
---269	16153065	2020	7	28	Fifer Street	withdraw	80
---288	25506511	2020	7	28	Fifer Street	withdraw	20
---313	81061156	2020	7	28	Fifer Street	withdraw	30
---336	26013199	2020	7	28	Fifer Street	withdraw	35
+--[account_number, person_id, creation_year, id, name, phone_number, passport, license_plate]
+--49610011	686048	2010	686048	Ernest	(367) 555-5533	5773159633	94KL13X
+--26013199	514354	2012	514354	Russell	(770) 555-1861	3592750733	322W7JE
+--16153065	458378	2012	458378	Roy	(122) 555-4581	4408372428	QX4YZN3
+--28296815	395717	2014	395717	Bobby	(826) 555-1652	9878712108	30G67EN
+--25506511	396669	2014	396669	Elizabeth	(829) 555-5269	7049073643	L93JTIZ
+--28500762	467400	2014	467400	Danielle	(389) 555-5198	8496433585	4328GD8
+--76054385	449774	2015	449774	Madison	(286) 555-6063	1988161715	1106N58
+--81061156	438727	2018	438727	Victoria	(338) 555-6650	9586786673	8X428L0
+
+------------------------------------------------------------------------
+-- Intersecting those who withdrew at Fifer Street from the listing of those
+-- who had a phone call for less than 1 minute and drove off within 10 min of the scene,
+-- both Ernest and Russell fit the description so one of them must be the thief.
+-- Now, with 3 more tables: flights, passengers, and airports, we can eliminate one of them
+
+ SELECT people.NAME
+FROM   phone_calls
+       INNER JOIN people
+               ON people.phone_number = phone_calls.caller
+WHERE  caller IN (SELECT phone_number
+                  FROM   people
+                  WHERE  license_plate IN (SELECT license_plate
+                                           FROM   courthouse_security_logs
+                                           WHERE  activity = 'exit'
+                                                  AND month = 7
+                                                  AND day = 28
+                                                  AND hour = 10
+                                                  AND minute >= 15
+                                                  AND minute <= 25))
+       AND year = 2020
+       AND month = 7
+       AND day = 28
+       AND duration < 60
+INTERSECT
+SELECT people.NAME
+FROM   bank_accounts
+       INNER JOIN people
+               ON people.id = bank_accounts.person_id
+WHERE  account_number IN (SELECT account_number
+                          FROM   atm_transactions
+                          WHERE  year = 2020
+                                 AND month = 7
+                                 AND day = 28
+                                 AND transaction_type = 'withdraw'
+                                 AND atm_location = 'Fifer Street');
