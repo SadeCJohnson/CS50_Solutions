90808747bff7c57ec5ab98b97a52eb0cd021b1e5
Kesler Mathieu <kesmathieu30@gmail.com>
2022-01-03 02:50:26 -0500

    adding post logic

--- Web_Lab9_Birthdays/application.py
+++ Web_Lab9_Birthdays/application.py
@@ -15,9 +15,11 @@ db = SQL("sqlite:///birthdays.db")
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-
-        # TODO: Add the user's entry into the database
-
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)",
+                    request.form.get('name'),
+                    request.form.get('month'),
+                    request.form.get('day')
+                   )
         return redirect("/")
 
     else:
