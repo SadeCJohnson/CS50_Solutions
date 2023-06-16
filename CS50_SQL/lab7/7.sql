f93cd7a42dbf1b738ad1624a8ccc5b4d3ef6dd66
Sadé C. Johnson <60587066+SadeCJohnson@users.noreply.github.com>
2023-06-16 12:11:53 -0500

    Removed Misc folders for the purposes of my use case

--- CS50_SQL/lab7/7.sql
+++
@@ -1,9 +0,0 @@
--- In 7.sql, write a SQL query that returns the average energy of songs that are by Drake.
--- Your query should output a table with a single column and a single row containing the average energy.
--- You should not make any assumptions about what Drake’s artist_id is.
-
-SELECT AVG(songs.energy)
-FROM   songs
-JOIN artists
-on artists.id = songs.artist_id
-WHERE artists.name is 'Drake';
--- CS50_SQL/pset7/6.sql
+++
@@ -1,7 +0,0 @@
--- In 6.sql, write a SQL query to determine the average rating of all movies released in 2012.
--- Your query should output a table with a single column and a single row (not including the header) containing the average rating.
-SELECT AVG(rating) AS ""
-FROM ratings
-JOIN movies
-on ratings.movie_id = movies.id
-WHERE movies.year = 2012
